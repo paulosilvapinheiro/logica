2530621e09134e131c5e254f2e15ada61ab5f716
Paulo da Silva Pinheiro <Paulosilvapinheiro@hotmail.com>
2022-11-03 00:09:26 -0300

    Ordenacao

--- console/Program.cs
+++ console/Program.cs
@@ -5,24 +5,33 @@
         static void Main(string[] args)
         {
 
-            fibonacci fib = new fibonacci();
+            //fibonacci fib = new fibonacci();
 
-            int[] seq = fib.Sequence(10);
-            Console.WriteLine("[{0}]", string.Join(" |", seq));
+            //int[] seq = fib.Sequence(10);
+            //Console.WriteLine("[{0}]", string.Join(" |", seq));
 
-            int resposta = 0;
+            //int resposta = 0;
 
-            seq = fib.Sequence(10, 10);
-            Console.WriteLine("[{0}]", string.Join(" |", seq));
+            //seq = fib.Sequence(10, 10);
+            //Console.WriteLine("[{0}]", string.Join(" |", seq));
 
-            while (resposta == 0)
-            {
-                resposta = fib.TesteNum(int.Parse(Console.ReadLine()));
-                Console.WriteLine("[{0}]", resposta);
-            }
+            //while (resposta == 0)
+            //{
+            //    resposta = fib.TesteNum(int.Parse(Console.ReadLine()));
+            //    Console.WriteLine("[{0}]", resposta);
+            //}
 
 
-            Console.Read();
+            //Console.Read();
+
+            Ordenacao ord = new Ordenacao();
+
+            Console.WriteLine("[{0}]", string.Join(" |", ord.OrdenacaoForma_1(new int[] { 10, 6, 3, 2, 8, 7 })));
+            Console.WriteLine("[{0}]", string.Join(" |", ord.OrdenacaoForma_2 (new int[] { 10, 6, 3, 2, 8, 7 })));
+            Console.WriteLine("[{0}]", string.Join(" |", ord.OrdenacaoForma_1(new int[] { 1, 10,  2, 4, 5,  9 })));
+            Console.WriteLine("[{0}]", string.Join(" |", ord.OrdenacaoForma_2(new int[] { 1, 10,  2, 4, 5,  9 })));
+            Console.WriteLine("[{0}]", string.Join(" |", ord.OrdenacaoForma_1(new int[] { 1 })));
+            Console.WriteLine("[{0}]", string.Join(" |", ord.OrdenacaoForma_2(new int[] { 1 })));
         }
     }
 }
