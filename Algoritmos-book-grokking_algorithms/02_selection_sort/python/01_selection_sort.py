d4be2fb45b7810585155f1c44edaf9e194307e66
paulo <paulo@paulo>
2024-09-30 18:19:08 -0300

    adicionado contador de voltar e testado

--- Algoritmos-book-grokking_algorithms/02_selection_sort/python/01_selection_sort.py
+++ Algoritmos-book-grokking_algorithms/02_selection_sort/python/01_selection_sort.py
@@ -1,3 +1,14 @@
+import sys 
+from pathlib import Path
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[2]
+sys.path.append(str(root))
+
+# from LapsCounter import *
+import LapsCounter as lc 
+
+lapsCounter = lc.LapsCounter()
+
 # Finds the smallest value in an array
 def findSmallest(arr):
   # Stores the smallest value
@@ -5,6 +16,8 @@ def findSmallest(arr):
   # Stores the index of the smallest value
   smallest_index = 0
   for i in range(1, len(arr)):
+    lapsCounter.volta()
+
     if arr[i] < smallest:
       smallest = arr[i]
       smallest_index = i
@@ -14,9 +27,12 @@ def findSmallest(arr):
 def selectionSort(arr):
   newArr = []
   for i in range(len(arr)):
+      lapsCounter.volta()
+
       # Finds the smallest element in the array and adds it to the new array
       smallest = findSmallest(arr)
       newArr.append(arr.pop(smallest))
   return newArr
 
-print selectionSort([5, 3, 6, 2, 10])
+print(f"Algoritmo SELECTION SORT (Array [20, 5, 3, 6, 2, 10]): Ordenando [ {selectionSort([ 5, 3, 6, 2, 10, 1, 11])} ] | Total de voltas [ {lapsCounter.totVoltas()} ]")
+# print (selectionSort([5, 3, 6, 2, 10]))
